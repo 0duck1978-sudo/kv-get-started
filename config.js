2acb43184746436dfd43affca431a2b54e54245b
kumsung_duckcraft <0duck1978@gmail.com>
2026-06-20 17:45:25 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: "https://uoctovqlmmwbanqfrwfk.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVvY3RvdnFsbW13YmFucWZyd2ZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4NjY2ODQsImV4cCI6MjA5NzQ0MjY4NH0.1jr3QYXQ9DwiNceI7Fdu0fk5V76_xnbDNOt-ex4V45Y",
+};
